6531e384f8e08bb81e0446db32197582d6c4b31e
Kamila Wosinek <kamila.wosinek@gmail.com>
2013-03-08 15:30:57 +0100

    create new projeckt

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var ofertyApp = angular.module('ofertyApp', [])
+  .config(['$routeProvider', function($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+ofertyApp.controller('MainCtrl', function($scope) {
+  $scope.awesomeThings = [
+    'HTML5 Boilerplate',
+    'AngularJS',
+    'Testacular'
+  ];
+});
