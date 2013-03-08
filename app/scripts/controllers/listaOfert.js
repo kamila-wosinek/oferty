92c53370deaffed9e73731cef151ae57d73b3b42
Kamila Wosinek <kamila.wosinek@gmail.com>
2013-03-08 17:18:20 +0100

    added exaple of list

--- app/scripts/controllers/listaOfert.js
+++ app/scripts/controllers/listaOfert.js
@@ -1,9 +1,14 @@
 'use strict';
 
 ofertyApp.controller('ListaOfertCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
+  $scope.ofert = [
+    {"name":"Test1","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test2","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test3","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test4","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test5","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test6","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test7","price":"Test","link":"Test","status":"Test"},
+    {"name":"Test8","price":"Test","link":"Test","status":"Test"}
   ];
 });
