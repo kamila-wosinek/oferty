5e2e5220bbabe732a5895f17a9c4951d40f39b1c
Kamila Wosinek <kamila.wosinek@gmail.com>
2013-03-09 19:02:16 +0100

    Binding function to save button

--- app/scripts/controllers/dodajOferty.js
+++ app/scripts/controllers/dodajOferty.js
@@ -1,14 +1,7 @@
 'use strict';
 
 ofertyApp.controller('DodajOfertyCtrl', function($scope) {
-  $scope.ofert = [
-    {"name":"Test1","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test2","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test3","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test4","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test5","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test6","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test7","price":"Test","link":"Test","status":"Test"},
-    {"name":"Test8","price":"Test","link":"Test","status":"Test"}
-  ];
+  $scope.add = function () {
+    alert('test');
+  };
 });
